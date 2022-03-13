b1bf8fcbcc0d3a366c602f521a93022c40ae138b
danielei9 <dbs99@hotmail.com>
2022-03-13 14:16:22 +0100

    seed post and commets

---
+++ server/seeders/template.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    // DECOMENTAR      (NAME TABLE , [{VALUES}])
+    // return queryInterface.bulkInsert('Blog',); //
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+  }
+};
